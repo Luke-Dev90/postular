92d382cacdc5ffdad1d72e18179fbad4a25fc2e5
Luke-Dev90 <lukee_sf@hotmail.com>
2021-03-15 18:18:24 -0300

    start proyect postular

---
+++ src/main/java/com/chalela/postular/PostularApplication.java
@@ -0,0 +1,13 @@
+package com.chalela.postular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostularApplication.class, args);
+	}
+
+}
